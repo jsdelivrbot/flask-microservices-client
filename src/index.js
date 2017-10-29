ae66bc860472fc1dcd2479f95740a9b838073d9f
rahulbansalmit <rahulfreescale@gmail.com>
2017-10-29 01:14:04 -0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -27,11 +27,7 @@ class App extends Component {
             <br/>
             <h1>All Users</h1>
             <hr/><br/>
-            {
-            this.state.users.map((user) => {
-              return <h4 key={user.id} className="well"><strong>{ user.username }</strong> - <em>{user.created_at}</em></h4>
-            })
-          }
+              <UsersList users={this.state.users}/>
           </div>
         </div>
       </div>
