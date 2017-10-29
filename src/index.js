0ec8f170fd7f481630cef543381786795dbb7408
rahulbansalmit <rahulfreescale@gmail.com>
2017-10-29 01:57:54 -0700

    add user

--- src/index.js
+++ src/index.js
@@ -8,31 +8,61 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      users: []
-  }
+  users: [],
+  username: '',
+  email: ''
+}
 }
   componentDidMount() {
     this.getUsers();
   }
+
+  handleChange(event) {
+  const obj = {};
+  obj[event.target.name] = event.target.value;
+  this.setState(obj);
+}
+
+ addUser(event) {
+  event.preventDefault();
+  const data = {
+    username: this.state.username,
+    email: this.state.email
+  }
+  axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+  .then((res) => {
+    this.getUsers();
+    this.setState({ username: '', email: '' });
+  })
+  .catch((err) => { console.log(err); })
+}
+
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
     .then((res) => { this.setState({ users: res.data.data.users }); })
     .catch((err) => { console.log(err); })
 }
   render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <br/>
-            <h1>All Users</h1>
-            <hr/><br/>
-              <UsersList users={this.state.users}/>
-          </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <br/>
+          <h1>All Users</h1>
+          <hr/><br/>
+          <AddUser
+             username={this.state.username}
+             email={this.state.email}
+             handleChange={this.handleChange.bind(this)}
+             addUser={this.addUser.bind(this)}
+           />
+          <br/>
+          <UsersList users={this.state.users}/>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 }
 
 ReactDOM.render(
