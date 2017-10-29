b0f63622ef5cc848e7ffaf992634ae31e4a10347
rahulbansalmit <rahulfreescale@gmail.com>
2017-10-29 01:11:34 -0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       users: []
-    }
   }
+}
   componentDidMount() {
     this.getUsers();
   }
@@ -17,20 +18,25 @@ class App extends Component {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
     .then((res) => { this.setState({ users: res.data.data.users }); })
     .catch((err) => { console.log(err); })
-  }
+}
   render() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <br/>
-          <h1>All Users</h1>
-          <hr/><br/>
-            <UsersList users={this.state.users}/>
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <br/>
+            <h1>All Users</h1>
+            <hr/><br/>
+            {
+            this.state.users.map((user) => {
+              return <h4 key={user.id} className="well"><strong>{ user.username }</strong> - <em>{user.created_at}</em></h4>
+            })
+          }
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 ReactDOM.render(
